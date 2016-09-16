86d40dbfaa237fec1376ddd2b87866bd1ae88b2f
Mark Keith <mark.r.keith@gmail.com>
2016-09-16 11:54:00 -0400

    Define ES5 debounce function w/ comments

--- debounce/debounce.js
+++ debounce/debounce.js
@@ -1,5 +1,22 @@
-const debounce = () => {
-  //function body
+var debounce = function (func, wait) {
+  var called = false;
+  var args;
+  var context;
+
+  return function() {
+    //Each time function is invoked, update args and context
+    args = [].slice.call(arguments);
+    context = this;
+
+    //Prevent function invocation while "waiting"
+    if(!called) {
+      called = true;
+      return setTimeout(function() {
+        func.apply(context, args);
+        called = false;
+      }, wait);
+    }
+  }
 }
 
 module.exports = debounce;
